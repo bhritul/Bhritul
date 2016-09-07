599c465374d1d5a748ad1eb178bff323277a2811
Bhritul <bhritul@programmer.net>
2016-09-07 14:32:30 -0400

    Checking in

--- Mamma/Mamma/Program.cs
+++ Mamma/Mamma/Program.cs
@@ -7,6 +7,11 @@ namespace Mamma
         public static void Main(string[] args)
         {
             Console.WriteLine("Mamma is running now.....");
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Console Is Writing - " + i.ToString());
+            }
+
             Console.ReadLine();
         }
     }

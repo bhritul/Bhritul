db1647713c142e7783a50c2e348f7a1bc13039be
Bhritul <bhritul@programmer.net>
2016-09-07 09:07:25 -0400

    Checking Console for the First Time

---
+++ Mamma/Mamma/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mamma
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Mamma is running now.....");
+            Console.ReadLine();
+        }
+    }
+}
